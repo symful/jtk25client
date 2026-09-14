ea775ec4ae05b84a649065c06daa035392bbcf33
symful <careday17@gmail.com>
2026-09-14 17:15:51 +0700

    fix: cache GoRouter in initState so theme change doesn't reset navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import 'core/router/router.dart';
 import 'core/theme/theme.dart';
 import 'features/settings/providers/theme_provider.dart';
 import 'main.dart' show scaffoldMessengerKey;
 
-/// Root widget for the JTK25 client application.
-class Jtk25App extends ConsumerWidget {
+class Jtk25App extends ConsumerStatefulWidget {
   const Jtk25App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Jtk25App> createState() => _Jtk25AppState();
+}
+
+class _Jtk25AppState extends ConsumerState<Jtk25App> {
+  late final GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _router = createRouter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final themeMode = ref.watch(themeModeProvider);
 
     return MaterialApp.router(
@@ -21,7 +34,7 @@ class Jtk25App extends ConsumerWidget {
       theme: buildJtk25Theme(),
       darkTheme: buildJtk25DarkTheme(),
       themeMode: themeMode.flutterThemeMode,
-      routerConfig: createRouter(),
+      routerConfig: _router,
     );
   }
 }
